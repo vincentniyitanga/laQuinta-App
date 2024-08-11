2bf77dd36bebe143f8a9acb726d9359b568710ae
vincentniyitanga <vincentniyitanga@mail.weber.edu>
2024-08-11 12:57:09 -0600

    Initial Commit

---
+++ laQuinta App/laQuinta_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  laQuinta_AppApp.swift
+//  laQuinta App
+//
+//  Created by Vincent Niyitanga on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct laQuinta_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ laQuinta AppUITests/laQuinta_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  laQuinta_AppUITestsLaunchTests.swift
+//  laQuinta AppUITests
+//
+//  Created by Vincent Niyitanga on 8/11/24.
+//
+
+import XCTest
+
+final class laQuinta_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
